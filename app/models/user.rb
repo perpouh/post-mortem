7d1980a98d8313f25eaeb02448ece108c8e42f77
perpouh <perpouh@gmail.com>
2020-11-18 14:41:05 +0900

    vueリストラ

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  
+  validate :allow_domain_list
+
+  def allow_domain_list
+    return if email.blank?
+    # 許可するドメインをどこで管理するかは後で考える。環境変数かDBか
+    errors.add(:email, "メールアドレスが不正です。システムに許可されたドメイン以外は登録できません。") unless email.end_with?("co.jp")
+  end
 end
