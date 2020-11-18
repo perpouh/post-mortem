d15f697f0d0b42610bc73388e7a7de13636af3d6
perpouh <perpouh@gmail.com>
2020-11-18 18:30:15 +0900

    Userモデルにusernameカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_11_16_083500) do
+ActiveRecord::Schema.define(version: 2020_11_18_092358) do
 
   create_table "bookmarks", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4", force: :cascade do |t|
     t.integer "user_id"
@@ -78,6 +78,7 @@ ActiveRecord::Schema.define(version: 2020_11_16_083500) do
     t.integer "failed_attempts", default: 0, null: false
     t.string "unlock_token"
     t.datetime "locked_at"
+    t.string "username"
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
