de57e5cb6668d465ba8b314be4d0929356d90386
perpouh <perpouh@gmail.com>
2020-11-25 11:58:44 +0900

    通信成功ケース。ここからbaseUrlをなんとかしたい

--- data/www/src/components/ProjectList.vue
+++ data/www/src/components/ProjectList.vue
@@ -3,23 +3,18 @@
 </template>
 
 <script>
-import axios from 'axios'
 export default{
   name: 'ProjectList',
   created(){
     this.fetchData()
   },
   watch: {
-    // ルートが変更されたらこのメソッドを再び呼び出します
     '$route': 'fetchData'
   },
   methods: {
     fetchData () {
-      // this.parents = [{id: 1, name:"がじゃりこ", company:"Calbee", children:[{id:1, name:"じゃがりこbits", price: 110},{id:2, name:"じゃがりこLサイズ", price: 135}]}]
-      axios.get("http://localhost:3030/projects")
+      this.$http.get("http://localhost:3030/projects")()
       .then(function(res){
-        //vueにバインドされている値を書き換えると表示に反映される
-        // app.result = res.data
         console.log(res)
       })
     }
