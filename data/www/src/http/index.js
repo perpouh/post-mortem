14ec4ad70d2dcb1d13fea8fd5b076d87abb1ccab
perpouh <perpouh@gmail.com>
2020-11-26 11:24:27 +0900

    APIのbaseURLを環境変数に出した

--- data/www/src/http/index.js
+++ data/www/src/http/index.js
@@ -1,6 +1,6 @@
 import Axios from 'axios'
 const axios = Axios.create({
-  baseURL: 'http://localhost:3030', // バックエンドB のURL:port を指定する
+  baseURL: process.env.VUE_APP_API_ENDPOINT,
   headers: {
     'Content-Type': 'application/json',
     'X-Requested-With': 'XMLHttpRequest'
