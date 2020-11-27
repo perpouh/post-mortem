7136df1dd4468841bc29d9a4a67cd7cbea712faa
perpouh <perpouh@gmail.com>
2020-11-27 14:11:24 +0900

    CORS設定

--- server/config/application.rb
+++ server/config/application.rb
@@ -33,15 +33,5 @@ module App
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    # Permit cross origin
-    config.middleware.insert_before 0, Rack::Cors do
-      allow do
-        origins ENV['CLIENT_HOST']
-        resource "*",
-          headers: :any,
-          methods: [:get, :post, :options, :head]
-      end
-    end
   end
 end
