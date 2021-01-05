2242bf2e0a865c5648b40ae04ba59ed0b2a92306
perpouh <perpouh@gmail.com>
2021-01-05 18:02:00 +0900

    API修正

--- server/app/controllers/my/tickets_controller.rb
+++ server/app/controllers/my/tickets_controller.rb
@@ -2,12 +2,11 @@ module My
   # 自分の参加しているもしくはウォッチしているプロジェクトのチケットを取得する
   class TicketsController < AuthenticatedController
     def newer
-      @tickets = Ticket.in_joining_project(current_user).page(params[:page] || 1).per(20)
-      # render json: @tickets, status: :ok
+      @tickets = Ticket.in_joining_project(current_user).tickets_only.page(params[:page] || 1).per(20)
     end
 
     def active
-      @tickets = Ticket.in_joining_project(current_user).active.page(params[:page] || 1).per(20)
+      @tickets = Ticket.in_joining_project(current_user).tickets_only.active.page(params[:page] || 1).per(20)
       render json: @tickets, status: :ok
     end
 
